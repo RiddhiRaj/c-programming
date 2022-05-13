5c5fc0e9a5a3c14cc58872793e20c83c5dc08715
RiddhiRaj <riddhirajg@gmail.com>
2022-05-14 01:58:54 +0530

    .

---
+++ C Programming/c-in-vscode/ASSIGNMENT 1-40/28. prime/prime.c
@@ -0,0 +1,23 @@
+//Write a program in C to determine whether a number is prime or not. 
+
+#include<stdio.h>
+int main()
+{
+    int n, i, flag = 0;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    for (i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+        printf("%d is a prime number\n", n);
+    else
+        printf("%d is not a prime number\n", n);
+    return 0;
+}
+
